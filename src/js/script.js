211991ac884ce6f5e51e57c4a4b8de9bd1926c5f
Zac Conlin <zac.conlin@gmail.com>
2022-09-11 22:14:18 -0600

    Almost done!

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
-// script.js: BYU IT&C 210a JavaScript
-
-// Insert the Lab 1b JavaScript here
+function on_submit(event) {
+    let formData = new FormData(event.currentTarget);
+    let json = JSON.stringify(Object.fromEntries(formData));
+    alert(json);
+    event.preventDefault();
+}
